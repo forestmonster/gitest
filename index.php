1e286c83c15d32690e3af93d38dd6f9b2cad6e37
Forest Monsen <forest.monsen@gmail.com>
2012-01-30 09:46:59 -0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+$var = 'blah';
+if ($var == 'blah') {
+  $john = array(
+    'color' => 'blue',
+    'type'  => 'paint',
+    'quantity' => '6',
+  );
+  print_r($john);
+  var_dump($john);
+}
